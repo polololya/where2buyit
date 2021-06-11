d8e1f1008f688b4ea2ad092bb1008048a4e81ae2
HveshO <85547530+HveshO@users.noreply.github.com>
2021-06-11 16:29:22 +0300

    Update SearchEngine.py

--- src/SearchEngine.py
+++ src/SearchEngine.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
+import sklearn.metrics.pairwise as sl
 
+def SimilarItem(target_embeding,dataset_embedings):
+    value_similarity=pd.DataFrame(np.squeeze(sl.cosine_similarity(target_embeding.values.reshape(1,-1), dataset_embedings.values)))
+    dataset_embedings.insert(0,'similar',value_similarity.values)
+    dataset_embedings=pd.DataFrame(dataset_embedings).sort_values(by='similar',ascending=False)
+    similarity5=dataset_embedings.iloc[:5]
+    return similarity5
+#dataset_embedings=pd.DataFrame(np.random.rand(100,128))
+#target_embeding=pd.DataFrame(np.random.rand(1,128))
+#similarity5=SimilarItem(target_embeding,dataset_embedings)
