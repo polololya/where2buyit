4e4cf9121cc33e4c766bc2ab2a460b28f6cb5e23
HveshO <85547530+HveshO@users.noreply.github.com>
2021-06-21 22:21:06 +0300

    Update SearchEngine.py

--- src/SearchEngine.py
+++ src/SearchEngine.py
@@ -2,63 +2,49 @@ import numpy as np
 import pandas as pd
 import sklearn.metrics.pairwise as sl
 import time
+import matplotlib.pyplot as plt
+import cv2
+import math
 # --------главная функция--------
-def SimilarItem(target_embeding, dataset_embedings):
+def SimilarItem(target_embeding, dataset_embedings,K):
     value_similarity = pd.DataFrame(
         np.squeeze(sl.cosine_similarity(target_embeding.values.reshape(1, -1), dataset_embedings)))
     # dataset_embedings.insert(0,'similar',value_similarity) - не делает копию объекта и удалить этот столбец тяжело
     dataset_embedings = dataset_embedings.assign(similar=value_similarity)  # создает дубликат
     dataset_embedings = pd.DataFrame(dataset_embedings).sort_values(by='similar', ascending=False)
-    similarity5 = dataset_embedings.iloc[:5]
-    return similarity5
-
+    similarityK = dataset_embedings.iloc[:K]
+    return similarityK
 
 # --------метрики--------
-PrecisionK=lambda similarity5_index,positive_index: len(set(similarity5_index)&set(positive_index))/len(similarity5_index)
-#PrecisionK работает только для одного целевого эмбеддинга, принимает вторым значением индексы реальных похожих предметов
-def AvgPrecisionK(target_embedings,dataset_embedings,positive_indices):
+PrecisionK=lambda similarityK_index,positive_index:len(set(similarityK_index)&set(positive_index))/len(similarityK_index)
+def AvgPrecisionK(target_embedings,dataset_embedings,positive_indices,K):
     value_similarity=pd.DataFrame(np.squeeze(sl.cosine_similarity(target_embedings, dataset_embedings)))
-    similarity5=pd.DataFrame(np.argsort(value_similarity)).iloc[:,-5:]
+    similarityK_indices=pd.DataFrame(np.argsort(value_similarity)).iloc[:,-K:]#получаем индексы
     n = len(target_embedings)
     k = np.zeros((n))
     for i in range(n):
-        k[i]=PrecisionK(similarity5.iloc[i], positive_indices.iloc[i])
+        k[i]=PrecisionK(similarityK_indices.iloc[i], positive_indices.iloc[i])
     k=sum(k)/n
     #по идее 24-27 быстрее, если target'ов много
     #k=np.array(list(map(lambda i: PrecisionK1(similarity5.iloc[i],positive_indices[i]),similarity5.index)))
     return k
-emb_size = 256
-dataset_size = 100
-
-df = pd.DataFrame(np.random.rand(dataset_size, emb_size))
-
-classes = ['cat','dog','horse','pig','hen']
-df['label'] = np.random.choice(classes, size=dataset_size)
-
-df['img_path'] = np.random.randint(low=100, high=1000, size=dataset_size).astype(str)
-df['img_path'] = 'C://dummy_folder/' + df['img_path'] + '.png'
-
-target=df.iloc[np.random.randint(0,len(df)),:emb_size]
-start_time = time.time()
-dataset=df.iloc[:,:emb_size]
-similarity5=SimilarItem(target, dataset)
-similarity5=similarity5.join(df.iloc[similarity5.index,emb_size:])
-print("--- %s seconds ---" % (time.time() - start_time))
-print(df.iloc[similarity5.index,:])
-print(similarity5)
-print("Столбцы не потерялись, SimilarItem работает, если подавать только значения без текста ")
-positive_index=np.random.choice(dataset.index,np.random.randint(1,len(dataset)),replace = False)
-#print(positive_index)
-start_time = time.time()
-K=PrecisionK(similarity5.index,positive_index)
-print("--- %s seconds ---" % (time.time() - start_time))
-print(K, ' - значение метрики для одного целевого изображения')
-targets=df.iloc[np.random.randint(len(df),size=np.random.randint(len(df))),:emb_size]#рандомные целевые векторы
-positive_indices=[]# индексы похожих изображений из базы для каждого целевого изображения в разных количествах
-for i in range (len(targets)):
-    positive_indices+=[np.random.choice(dataset.index,np.random.randint(1,len(dataset)),replace = False)]
-positive_indices=pd.DataFrame(positive_indices)
-start_time = time.time()
-k=AvgPrecisionK(targets,dataset,positive_indices)
-print("--- %s seconds ---" % (time.time() - start_time))
-print(k,' - усредненное значение метрики')
+def similar_visual(similarityK,column):#принимает эмбединги похожих и столбец в котором хранится путь к изображениям
+    pic_box = plt.figure()
+    i=1
+    n=len(similarityK)
+    col = int(math.sqrt(n))
+    row = math.ceil(n / col)
+    for value in similarityK.loc[:, column]:
+        #imread не читает путь с русскими буквами
+        picture = cv2.imread(value)
+        # конвертируем BGR изображение в RGB
+        picture = cv2.cvtColor(picture, cv2.COLOR_BGR2RGB)
+        # добавляем ячейку в pix_box для вывода текущего изображения
+        pic_box.add_subplot(row, col, i)
+        plt.imshow(picture)
+        # отключаем отображение осей
+        plt.axis('off')
+        i+=1
+    pic_box.tight_layout(pad=0, w_pad=0, h_pad=0)
+    # выводим все созданные фигуры на экран
+    plt.show()
