cfb68b84b61fd64d291d0bd8b2f3af608a431c72
HveshO <85547530+HveshO@users.noreply.github.com>
2021-06-29 18:33:10 +0300

    Update script_embedding_var2.py

--- notebooks/script_embedding_var2.py
+++ notebooks/script_embedding_var2.py
@@ -1,38 +1,28 @@
-from HardTripletLoss import EmbeddingModel
 import pandas as pd
 import tensorflow as tf
+import numpy as np
+from PIL import Image
 DATASET_DIR='Stanford_Online_Products/'
-FULL_DATASET_FILE = 'Ebay_info.txt'
+FULL_DATASET_FILE = 'Ebay_train.txt'
 TARGET_SHAPE=(128, 128)
 SAVE_FILE='df_emb'
 PATH_MODEL='Model/hard_model'
 
 #Второй вариант скрипта: считывание происходит по текстовому файлу, который пробегается по всей директории
-
-def preprocess_image(TARGET_SHAPE,filename: tf.Tensor):
-    """
-    Загрузка изображения, декодирование, перевод значений в числа с плавающей точкой, а также изменение размера
-    """
-
-    image_string = tf.io.read_file(filename)
-    image = tf.image.decode_jpeg(image_string, channels=3)
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    image = tf.image.resize(image, TARGET_SHAPE)
-    return image
 full_dataset = pd.read_csv(f'{DATASET_DIR}{FULL_DATASET_FILE}', sep=' ')
 embeddings=pd.DataFrame([])
-n=len(full_dataset)
-#n=10
+#n=len(full_dataset)
+n=30000
 embedding_model = EmbeddingModel(target_shape=TARGET_SHAPE)
 embedding_model.load_weights(PATH_MODEL)
+print(full_dataset)
 for i in range (n):#работает очень медленно, но я не знаю как оптимизировать
-    ret=tf.Variable(DATASET_DIR+'/'+full_dataset.iloc[i,-1],dtype=tf.string)
-    im=preprocess_image(TARGET_SHAPE,ret)
-    embeddings =embeddings.append(pd.DataFrame(embedding_model.predict(tf.expand_dims(im, axis=0))),ignore_index=True)
-    #test_model. если бы была бы embedding_model, то было бы embedding_model.
+    im = DATASET_DIR+str(full_dataset.iloc[i]['path'])
+    image = tf.keras.preprocessing.image.load_img(im)
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr=tf.image.resize(input_arr, TARGET_SHAPE)
+    input_arr = np.array([input_arr])
+    embeddings =embeddings.append(pd.DataFrame(embedding_model.predict(input_arr)),ignore_index=True)
 df=embeddings.join(full_dataset.iloc[:n,-3:])
 df.columns = df.columns.astype (str)
 df.to_parquet(SAVE_FILE,engine='fastparquet')
-#pf=pd.read_parquet(SAVE_FILE,engine='fastparquet')
-
-
