cf63ce061ca51bf50372b24a99a6ff20f7a13cfe
HveshO <85547530+HveshO@users.noreply.github.com>
2021-07-02 14:48:28 +0300

    Update script_embedding_var2.py

--- notebooks/script_embedding_var2.py
+++ notebooks/script_embedding_var2.py
@@ -1,24 +1,28 @@
-from HardTripletLoss import EmbeddingModel
 import pandas as pd
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-DATASET_DIR='Stanford_Online_Products/'
+from Model import EmbeddingModel
+
+DATASET_DIR='/content/Stanford_Online_Products'
 FULL_DATASET_FILE = 'Ebay_train.txt'
 TARGET_SHAPE=(128, 128)
 SAVE_FILE='df_emb'
-PATH_MODEL='Model/hard_model'
+PATH_MODEL='/content/drive/MyDrive/ColabNotebooks/hard_model_v2'
+DATASET_DIR='/content/Stanford_Online_Products/'
+COLUMN_WITH_PATH='path'
 
 #Второй вариант скрипта: считывание происходит по текстовому файлу, который пробегается по всей директории
 full_dataset = pd.read_csv(f'{DATASET_DIR}{FULL_DATASET_FILE}', sep=' ')
 embeddings=pd.DataFrame([])
-#n=len(full_dataset)
-n=30000
+n=len(full_dataset)
+#n=30000
 embedding_model = EmbeddingModel(target_shape=TARGET_SHAPE)
 embedding_model.load_weights(PATH_MODEL)
-print(full_dataset)
-for i in range (n):#работает очень медленно, но я не знаю как оптимизировать
-    im = DATASET_DIR+str(full_dataset.iloc[i]['path'])
+for i in range (n):
+    #if (i%100==0):
+      #print(i)
+    im = DATASET_DIR+str(full_dataset.iloc[i][COLUMN_WITH_PATH])
     image = tf.keras.preprocessing.image.load_img(im)
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr=tf.image.resize(input_arr, TARGET_SHAPE)
@@ -27,3 +31,5 @@ for i in range (n):#работает очень медленно, но я не �
 df=embeddings.join(full_dataset.iloc[:n,-3:])
 df.columns = df.columns.astype (str)
 df.to_parquet(SAVE_FILE,engine='fastparquet')
+#pf=pd.read_parquet(SAVE_FILE,engine='fastparquet')
+#print(pf)
